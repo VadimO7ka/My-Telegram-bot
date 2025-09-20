d9caefc525fe038b63a0213b3e273437fe06788e
Vadim07ka <146621679+VadimO7ka@users.noreply.github.com>
2025-09-20 19:28:46 +0700

    Delete env.py

--- env.py
+++
@@ -1,6 +0,0 @@
-# from dotenv import load_dotenv
-# import os
-
-# load_dotenv()
-# TOKEN = os.getenv("7948578566:AAGZzyXFx4vgmAZTsRELno1heKLK--B1OhY")
-# CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", "60"))
